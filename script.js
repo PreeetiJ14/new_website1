62013d1a1922efc2e58cf7bc1320b7cd7b9d16c7
PreeetiJ14 <163393570+PreeetiJ14@users.noreply.github.com>
2024-06-26 14:44:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Filter-items css starts
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for (let i = 0; i < sortBtn.length; i++) {
+    sortBtn[i].addEventListener('click', function () {
+        for (let j = 0; j < sortBtn.length; j++) {
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+
+
+        let targetData = this.getAttribute('data-target');
+
+        for (let k = 0; k < sortItem.length; k++) {
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if (sortItem[k].getAttribute('data-item') == targetData || targetData == "all") {
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
